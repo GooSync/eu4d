f6f13c4a47e24512d22d146ff743dd195ce70e64
AZhilenkov <azhilenkov@tangem.com>
2020-03-11 12:39:55 +0300

    - extend RvVH

--- lib/src/main/java/ru/dev/gbixahue/eu4d/lib/android/_android/views/recycler_view/holder.kt
+++ lib/src/main/java/ru/dev/gbixahue/eu4d/lib/android/_android/views/recycler_view/holder.kt
@@ -7,30 +7,25 @@ import ru.dev.gbixahue.eu4d.lib.kotlin.common.DataBinder
 /**
  * Created by Anton Zhilenkov on 17.11.17.
  */
-abstract class RvBaseVH<D>(itemView: View): RecyclerView.ViewHolder(itemView), DataBinder<D>
+abstract class RvBaseVH<D>(itemView: View): RecyclerView.ViewHolder(itemView), DataBinder<D> {
+  abstract fun onDataBound(data: D)
+}
 
 
 
 abstract class RvVH<D: Any>(
     itemView: View,
     protected val callback: RvCallback<D>? = null
-): RvBaseVH<D>(itemView), View.OnClickListener {
+): RvBaseVH<D>(itemView) {
 
   protected lateinit var itemData: D
 
-  init {
-    setOnClickListenerToView(itemView)
-  }
-
-  protected fun setOnClickListenerToView(view: View) {
-    view.setOnClickListener(this)
-  }
-
-  override fun onClick(v: View?) {
-    callback?.invoke(itemViewType, adapterPosition, itemData)
-  }
-
   override fun bindData(data: D) {
     itemData = data
+    onDataBound(itemData)
+  }
+
+  protected fun invokeCallback() {
+    callback?.invoke(itemViewType, adapterPosition, itemData)
   }
 }
